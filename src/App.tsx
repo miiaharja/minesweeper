c1b29ea71038270b5cf9741423ba355a8ae028d1
miiaharja <miia.harja@gmail.com>
2022-11-03 19:02:48 +0200

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
+import { Box, Button } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import "./App.css";
-import reactLogo from "./assets/react.svg";
 
 export function App() {
   const [count, setCount] = useState(0);
@@ -12,28 +11,10 @@ export function App() {
   }, [count]);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          type="button"
-          onClick={() => setCount((prevCount) => prevCount + 1)}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">{t("general.save")}</p>
-    </div>
+    <Box>
+      <Button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        {t("temp.counter-count", { count })}
+      </Button>
+    </Box>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./i18n/i18n";
-import "./index.css";
 import { customTheme } from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
