5184364854b31135f17ebc2685f059471f1829aa
miiaharja <miia.harja@gmail.com>
2022-11-04 10:22:49 +0200

    Added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,5 @@
-import { Box, Button } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import { useTranslation } from "react-i18next";
+import { Navbar } from "./components/UI/Navbar";
 
 export function App() {
-  const [count, setCount] = useState(0);
-  const { t } = useTranslation();
-
-  useEffect(() => {
-    console.log(count);
-  }, [count]);
-
-  return (
-    <Box>
-      <Button onClick={() => setCount((prevCount) => prevCount + 1)}>
-        {t("temp.counter-count", { count })}
-      </Button>
-    </Box>
-  );
+  return <Navbar />;
 }
---
+++ src/components/UI/Navigation/MobileNav.tsx
@@ -0,0 +1,17 @@
+import { Stack, useColorModeValue } from "@chakra-ui/react";
+import { NAV_ITEMS } from "../Navbar";
+import { MobileNavItem } from "./MobileNavItem";
+
+export function MobileNav() {
+  return (
+    <Stack
+      bg={useColorModeValue("white", "gray.800")}
+      p={4}
+      display={{ md: "none" }}
+    >
+      {NAV_ITEMS.map((navItem) => (
+        <MobileNavItem key={navItem.label} {...navItem} />
+      ))}
+    </Stack>
+  );
+}
